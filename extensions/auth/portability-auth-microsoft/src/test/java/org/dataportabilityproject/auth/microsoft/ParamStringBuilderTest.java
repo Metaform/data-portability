e7d4a96d2b8b389a1f0e3c38d9d58ea71dfa598c
Jim Marino <jim.marino@gmail.com>
2018-01-18 16:56:42 +0100

    implement OAuth generator for Microsoft extensions using the Microsoft Graph API

--- extensions/auth/portability-auth-microsoft/src/test/java/org/dataportabilityproject/auth/microsoft/ParamStringBuilderTest.java
+++ extensions/auth/portability-auth-microsoft/src/test/java/org/dataportabilityproject/auth/microsoft/ParamStringBuilderTest.java
@@ -6,35 +6,35 @@ import org.junit.Test;
 /**
  *
  */
-public class QueryPartBuilderTest {
+public class ParamStringBuilderTest {
 
     @Test
     public void verifySingleParam() {
-        QueryPartBuilder builder = new QueryPartBuilder();
+        ParamStringBuilder builder = new ParamStringBuilder();
         builder.startParam("param1").value("value1").endParam();
-        Assert.assertEquals("?param1=value1", builder.build());
+        Assert.assertEquals("param1=value1", builder.build());
     }
 
     @Test
     public void verifyMultipleParams() {
-        QueryPartBuilder builder = new QueryPartBuilder();
+        ParamStringBuilder builder = new ParamStringBuilder();
         builder.startParam("param1").value("value1").endParam();
         builder.startParam("param2").value("value2").endParam();
-        Assert.assertEquals("?param1=value1&param2=value2", builder.build());
+        Assert.assertEquals("param1=value1&param2=value2", builder.build());
     }
 
     @Test
     public void verifyMultipleValues() {
-        QueryPartBuilder builder = new QueryPartBuilder();
+        ParamStringBuilder builder = new ParamStringBuilder();
         builder.startParam("param1").value("value1").value("value2").endParam();
-        Assert.assertEquals("?param1=value1%20value2", builder.build());
+        Assert.assertEquals("param1=value1%20value2", builder.build());
     }
 
     @Test
     public void verifyEncodedValues() {
-        QueryPartBuilder builder = new QueryPartBuilder();
+        ParamStringBuilder builder = new ParamStringBuilder();
         builder.startParam("param1").value("value1&").value("value2&").endParam();
-        Assert.assertEquals("?param1=value1%26%20value2%26", builder.build());
+        Assert.assertEquals("param1=value1%26%20value2%26", builder.build());
     }
 
 }
