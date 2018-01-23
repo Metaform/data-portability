a5f1aadd7d82ba9e2ca3540a4ac6b4377f7694fd
Jim Marino <jmarino@metaformsystems.com>
2018-01-23 15:40:15 +0100

    De/serialize vCards in a single stream

--- extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/dataportabilityproject/transfer/microsoft/contacts/MicrosoftContactsImporter.java
+++ extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/dataportabilityproject/transfer/microsoft/contacts/MicrosoftContactsImporter.java
@@ -1,17 +1,32 @@
 package org.dataportabilityproject.transfer.microsoft.contacts;
 
+import ezvcard.VCard;
+import ezvcard.io.json.JCardReader;
 import org.dataportabilityproject.spi.transfer.provider.ImportResult;
 import org.dataportabilityproject.spi.transfer.provider.Importer;
 import org.dataportabilityproject.types.transfer.auth.TokenAuthData;
 import org.dataportabilityproject.types.transfer.models.contacts.ContactsModelWrapper;
 
+import java.io.IOException;
+import java.util.List;
+
 /**
  *
  */
 public class MicrosoftContactsImporter implements Importer<TokenAuthData, ContactsModelWrapper> {
+    private static final String CONTACTS_URL = "https://graph.microsoft.com/v1.0/me/contacts";
 
     @Override
-    public ImportResult importItem(TokenAuthData authData, ContactsModelWrapper data) {
-        return null;
+    public ImportResult importItem(TokenAuthData authData, ContactsModelWrapper wrapper) {
+        JCardReader reader = new JCardReader(wrapper.getVCards());
+        try {
+            List<VCard> cards = reader.readAll();
+
+            return new ImportResult(ImportResult.ResultType.OK);
+        } catch (IOException e) {
+            // TODO log
+            e.printStackTrace();
+            return new ImportResult(ImportResult.ResultType.ERROR, "Error deserializing contacts: " + e.getMessage());
+        }
     }
 }
